7a5d19e3857942938c93c45bf4146db21192c753
Mohib-76 <72691474+Mohib-76@users.noreply.github.com>
2021-06-03 10:36:27 +0530

    changed the screen

--- index.js
+++ index.js
@@ -6,35 +6,35 @@ for(var i=0 ; i<buttons.length; i++){
   buttons[i].addEventListener("click", function(event){
     // console.log(this.innerHTML);
     if(this.innerHTML != "=" && this.innerHTML != "C"){
-      screen1.innerHTML += this.innerHTML;
+      screen1.value += this.innerHTML;
     }
     else if (this.innerHTML === "="){
-      screen2.innerHTML = "= " + eval(screen1.innerHTML);
+      screen2.value = "= " + eval(screen1.value);
     }
     else if (this.innerHTML === "C"){
-      screen1.innerHTML = "";
-      screen2.innerHTML = "";
+      screen1.value = "";
+      screen2.value = "";
     }
   });
 }
 
-var keys = ["1", "2", "3" ,"4" , "5", "6", "7", "8", "9", "0", "+", "-", "*", "/", "." , "(" , ")"];
+// var keys = ["1", "2", "3" ,"4" , "5", "6", "7", "8", "9", "0", "+", "-", "*", "/", "." , "(" , ")"];
 
 document.addEventListener("keydown" , function(event){
     // console.log(event.key);
-    for(var i=0;i<keys.length;i++){
-      if(event.key === keys[i]){
-        screen1.innerHTML += event.key;
-      }
+    // for(var i=0;i<keys.length;i++){
+    //   if(event.key === keys[i]){
+    //     screen1.innerHTML += event.key;
+    //   }
+    // }
+    // if(event.key === "Backspace"){
+    //   screen1.innerHTML = screen1.innerHTML.substr(0,screen1.innerHTML.length-1);
+    // }
+    if (event.key === "=" || event.key === "Enter"){
+      screen2.value = "= " + eval(screen1.value);
     }
-    if(event.key === "Backspace"){
-      screen1.innerHTML = screen1.innerHTML.substr(0,screen1.innerHTML.length-1);
-    }
-    else if (event.key === "=" || event.key === "Enter"){
-      screen2.innerHTML = "= " + eval(screen1.innerHTML);
-    }
-    else if (event.key === "C"){
-      screen1.innerHTML = "";
-      screen2.innerHTML = "";
-    }
+    // else if (event.key === "C"){
+    //   screen1.innerHTML = "";
+    //   screen2.innerHTML = "";
+    // }
   });
